64973c9a5df764b11219e1ac34ea9979da74fa95
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-07-03 10:37:31 -0400

    added environment variables

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('Escape Hatch', 'postgres', process.env.DBPASS, {
+const sequelize = new Sequelize(process.env.PGDB, process.env.PGUSER, process.env.DBPASS, {
     dialect: 'postgres'
 })
 
