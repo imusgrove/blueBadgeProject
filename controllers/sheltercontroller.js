a14ef34a623006a498da82ac7cfe2ce352d8b728
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-09-14 23:17:54 -0400

    initial commit

--- controllers/sheltercontroller.js
+++ controllers/sheltercontroller.js
@@ -5,16 +5,16 @@ const Shelter = sequelize.import('../models/shelter');
 //create new Shelter log
 router.post('/create', (req, res)=> {
     Shelter.create({
-        shelter_name : req.body.shelterName,
-        shelter_number : req.body.shelterNumber,        
+        shelter_name : req.body.shelter_name,
+        shelter_number : req.body.shelter_number,        
         email : req.body.email,
         address : req.body.address,
         city : req.body.city,
-        state_init : req.body.stateInit,
-        zip_code : req.body.zipCode,
-        shelter_contact : req.body.shelterContact,
-        shelter_counsel : req.body.shelterCounsel, 
-        shelter_drug_counsel : req.body.shelterDrugCounsel,
+        state_init : req.body.state_init,
+        zip_code : req.body.zip_code,
+        shelter_contact : req.body.shelter_contact,
+        shelter_counsel : req.body.shelter_counsel, 
+        shelter_drug_counsel : req.body.shelter_drug_counsel,
         capacity : req.body.capacity,
         occupancy : req.body.occupancy
     })
@@ -28,49 +28,77 @@ router.post('/create', (req, res)=> {
 
 //allow shelter to be updated
 router.put("/update/:id", (req, res) => {
+    //res.send(200,{"hi":"hi"})
+
     var data = req.params.id;
-    var shelterName = req.body.shelterName;
-    var shelterNumber = req.body.shelterNumber;
+    var shelter_name = (typeof req.body.shelter_name === "String") ? req.body.shelter_name : "";
+    var shelter_number = req.body.shelter_number;
     var email = req.body.email;
     var address = req.body.address;
     var city = req.body.city;
-    var stateInit = req.body.stateInit;
-    var zipCode = req.body.zipCode;
-    var shelterContact = req.body.shelterContact;
-    var shelterCounsel = req.body.shelterCounsel;
-    var shelterDrugCounsel = req.body.shelterDrugCounsel;
+    var state_init = req.body.state_init;
+    var zip_code = req.body.zip_code;
+    var shelter_contact = req.body.shelter_contact;
+    var shelter_counsel = req.body.shelter_counsel;
+    var shelter_drug_counsel = req.body.shelter_drug_counsel;
     var capacity = req.body.capacity;
     var occupancy = req.body.occupancy;
 
+    const nextValues = {
+        shelter_name: shelter_name,
+        shelter_number: shelter_number,
+        email: email,
+        address: address,
+        city: city,
+        state_init: state_init,
+        zip_code: zip_code,
+        shelter_contact: shelter_contact,
+        shelter_counsel: shelter_counsel,
+        shelter_drug_counsel: shelter_drug_counsel,
+        capacity: capacity,
+        occupancy: occupancy
+    }
+
     Shelter.update(
+        nextValues,
+        {where:{id: data}}
+    ).then((affectedCount, affectedRows) => {
+        console.log("AFFECTED STUFF", affectedCount, affectedRows)
+        if(affectedRows === 1) {
+            res.send(200, nextValues)
+        }
+    })
+    /*Shelter.update(
         {
-            shelter_name: shelterName,
-            shelter_number: shelterNumber,
+            shelter_name: shelter_name,
+            shelter_number: shelter_number,
             email: email,
             address: address,
             city: city,
-            state_init: stateInit,
-            zipCode: zipCode,
-            shelter_contact: shelterContact,
-            shelter_counsel: shelterCounsel,
-            shelter_drug_counsel: shelterDrugCounsel,
+            state_init: state_init,
+            zip_code: zip_code,
+            shelter_contact: shelter_contact,
+            shelter_counsel: shelter_counsel,
+            shelter_drug_counsel: shelter_drug_counsel,
             capacity: capacity,
             occupancy: occupancy
         },
         {where:{id: data}}
-    ).then(
+    ).then((affectedCount, affectedRows) => {
+        console.log('done')
+    })*//*then(
         function updateSuccess(updatedProfile){
             res.json({
-                shelter_name: shelterName,
-                shelter_number: shelterNumber,
+                shelter_name: shelter_name,
+                shelter_number: shelter_number,
                 email: email,
                 address: address,
                 city: city,
-                state_init: stateInit,
-                zipCode: zipCode,
-                shelter_contact: shelterContact,
-                shelter_counsel: shelterCounsel,
-                shelter_drug_counsel: shelterDrugCounsel,
+                state_init: state_init,
+                zip_code: zip_code,
+                shelter_contact: shelter_contact,
+                shelter_counsel: shelter_counsel,
+                shelter_drug_counsel: shelter_drug_counsel,
                 capacity: capacity,
                 occupancy: occupancy
             });
@@ -78,7 +106,7 @@ router.put("/update/:id", (req, res) => {
         function updateError(err){
             res.send(500, err.message);
         }
-    );
+    );*/
 });
 
 //get all shelters
